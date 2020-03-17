8d49932bbfee8f2636f578afa5aae76215110166
tod619 <trevorodwyer619@hotmail.com>
2020-03-17 15:20:04 +0000

    Add variables to app.js file

---
+++ app.js
@@ -0,0 +1,5 @@
+// Variables
+const toggle = document.getElementById("toggle");
+const close = document.getElementById("close");
+const open = document.getElementById("open");
+const model = document.getElementById("modal");
