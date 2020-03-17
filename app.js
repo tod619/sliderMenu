8ab03e027795831841f85183f37f69bda9319e4a
tod619 <trevorodwyer619@hotmail.com>
2020-03-17 15:24:57 +0000

    Add eventListner to toggle btn

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ const toggle = document.getElementById("toggle");
 const close = document.getElementById("close");
 const open = document.getElementById("open");
 const model = document.getElementById("modal");
+
+// Toggle Navigation Bar
+toggle.addEventListener("click", () =>
+  document.body.classList.toggle("show-nav")
+);
